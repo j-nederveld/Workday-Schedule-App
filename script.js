5011beaac919285cf90a5eaba7ced65c6be33c28
John Nederveld <john.nederveld@gmail.com>
2020-09-17 20:02:21 -0400

    added functionality to save and retrieve input values

--- script.js
+++ script.js
@@ -1,23 +1,90 @@
 // var dateEl = document.querySelector("#showDate");
 var currentDate = moment().format("dddd, MMMM Do" );
-console.log(currentDate);
 
+// get hour
+var currentHour = moment().format("HH");
+console.log(currentHour);
+//define some global variables so they can be manipulated throughout the code
+var savedContent = [];
+let hourRow = '';
+let hourCol = '';
+let textArea = '';
+let saveText = '';
+
+//set the element with id "#currentDay" to equal the currentDate variable
 $("#currentDay").text(currentDate);
 
 var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM",];
 var hourContainer
 
-        for(i = 0; i < hours.length; i++) {
-      
-        let hourRow = $("<div>");
-      // 3. Then give each "letterBtn" the following classes: "letter-button" "letter" "letter-button-color".
-        hourRow.addClass("row hour")
-      // 4. Then give each "letterBtn" an attribute called "data-letter", with a value eqaual to "letters[i]"
-        hourRow.attr("data-hour", hours[i])
-      // 5. Then give each "letterBtn" a text equal to "letters[i]".
-        hourRow.text(hours[i])
+      //begin for loop to create elements based on the "hours" array
+  for(i = 0; i < hours.length; i++) {
 
-          
-      // 6. Finally, append each "letterBtn" to the "#buttons" div (provided).
+      //create the html elements we need. a row for each hour, with 3 colums for the hour #, textarea, and button so save the content of our textarea
+        let hourRow = $("<form>");
+        let hourCol = $("<div>");
+        let textArea = $("<textarea>");
+        let saveText = $("<button>");
+
+      //give each "letterBtn" the following classes: "letter-button" "letter" "letter-button-color".
+        hourRow.addClass("row hour form")
+        hourCol.addClass("col-1")
+        textArea.addClass("col-10 textArea")
+        textArea.attr('id', parseInt(i + 9));
+        saveText.addClass("col-1 saveBtn")
+
+      //give each "hourRow" an attribute called "data-hour", with a value eqaual to "hours[i]"
+        // textArea.attr("data-hour", parseInt(i + 9));
+        // saveText.attr("data-hour", parseInt(i + 9));
+        hourRow.attr("data-hour", parseInt(i + 9));
+
+
+
+      //add the hour variable to the correct row
+        hourCol.text(hours[i])
+
+      //add the row to our inner container, and then add each column to that row
           $("#innerContainer").append(hourRow);
+          hourRow.append($(hourCol));
+          hourRow.append($(textArea));
+          hourRow.append($(saveText));
+          saveText.text("");
+
+        if (parseInt(textArea.attr("data-hour")) > currentHour) {
+            textArea.addClass("future");
+          }
+        if (textArea.attr("data-hour") === currentHour) {
+          textArea.addClass("present");
         }
+         if (parseInt(textArea.attr("data-hour")) < currentHour) {
+          textArea.addClass("past");
+         }
+  }
+
+
+ 
+    for (i = 0; i < localStorage.length; i++) {
+      var keyName = localStorage.key(i);
+      console.log(keyName);
+  //get the value of our variable keyName
+      var value = localStorage.getItem(keyName);
+      value = value.slice(1, -1);
+      console.log(value); 
+  //create li with the text content of our Key --> Value
+      var storedInput = document.getElementById(keyName);
+      storedInput.value = value;
+      }
+
+  
+
+  $(".form").submit(function(e) {
+    e.preventDefault();
+    //get the hour that is being manipulated
+    var current = $(this).attr("data-hour");
+    //get the textarea that matches the same hour
+    var txt = document.getElementById(current);
+    var txt = txt.value;
+
+    localStorage.setItem(current, JSON.stringify(txt));
+}
+);
